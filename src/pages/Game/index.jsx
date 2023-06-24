25c2d1e6343d3c2e5ec62b3d088dd0cfc86e1ee1
Christian <c.espinoza.code@gmail.com>
2023-06-24 08:15:53 -0500

    Input counter implemented

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,9 +1,15 @@
+import './styles.css'
+
 function Game() {
   return (
-    <div>
+    <div className='game-container'>
       <ul>
         <li><input type="checkbox" className='game-shield' /></li>
+        <li><input type="checkbox" className='game-shield' /></li>
+        <li><input type="checkbox" className='game-shield' /></li>
+        <li><input type="checkbox" className='game-shield' /></li>
       </ul>
+      <h3 className='total-count'>Score: </h3>
     </div>
   )
 }
